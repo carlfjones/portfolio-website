3481fd95b48bf814bb58b47e41def48be3894bb1
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2020-05-13 13:35:34 +0100

    Navbar color changes with js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,33 @@
 const hamburger_checkbox = document.getElementById('hamburger');
 const navbar = document.querySelector('.navbar');
+const nav_link = document.getElementsByClassName('navbar__link');
+const sections = document.getElementsByClassName('section')
 
 hamburger_checkbox.addEventListener('change', function() {
     navbar.classList.toggle('active');
 })
+
+function menuColorChanger() {
+    for (let section of sections) {
+        let section_boundary = section.getBoundingClientRect();
+        for (let item of nav_link) {
+            let item_boundary = item.getBoundingClientRect();
+
+            if (item_boundary.y >= section_boundary.y && item_boundary.y + item_boundary.height <= section_boundary.y + section_boundary.height) {
+                let color = section.dataset.color;
+
+                switch (color) {
+                    case 'light':
+                        item.classList.add('light');
+                        break;
+                    case 'dark':
+                        item.classList.remove('light');
+                        break;
+                }
+            }
+        }
+    }
+}
+
+document.addEventListener('scroll', menuColorChanger);
+
